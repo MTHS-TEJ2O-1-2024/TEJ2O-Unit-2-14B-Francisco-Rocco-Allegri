97007e454dabb85c4ab97785dc7e978be2572f36
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-31 11:03:09 -0400

    placed fill-in pixel code and changed readme file

--- main.ts
+++ main.ts
@@ -2,7 +2,41 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program shows while loops in action
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
