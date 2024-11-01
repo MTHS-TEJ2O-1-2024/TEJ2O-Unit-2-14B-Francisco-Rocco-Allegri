3a8c3d8c35d343879cc0e171e01e256fa0ef77b0
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-31 21:12:15 -0400

    completed pixel border movement code

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Francisco Rocco Allegri
  * Created on: Oct 2024
- * This program shows while loops in action
+ * This program shows while loops in action, moving 
 */
 
 basic.clearScreen()
@@ -12,15 +12,28 @@ let loopCounter = 0
 
 basic.showIcon(IconNames.Happy)
 basic.pause(500)
-basic.clearScreen()
 
+//When the button A is pressed, the while loops will function, moving the pixel sprite.
+//It is moved in a clockwise fashion
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    loopCounter = 0
+   
+    loopCounter = 0 
     sprite = game.createSprite(0, 0)
-
     while (loopCounter <= 5) {
+        //sprite.set function  (X, value) will set a value for X (horizontal) 
+        //each loop will increment the value for X, moving it to the RIGHT towards FIVE
         sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 0
+    sprite = game.createSprite(4, 0)
+    while (loopCounter <= 5) {
+        //sprite.set function  (Y, value) will set a value for Y (vertical) 
+        //each loop will increment the value for Y, moving it DOWNWARDS towards FIVE
         sprite.set(LedSpriteProperty.Y, loopCounter)
         loopCounter = loopCounter + 1
         basic.pause(500)
@@ -28,15 +41,81 @@ input.onButtonPressed(Button.A, function () {
     sprite.delete()
 
     loopCounter = 5
-    sprite = game.createSprite(5, 5)
-
+    sprite = game.createSprite(4, 4)
     while (loopCounter >= 0) {
+        //sprite.set function  (X, value) will set a value for X (horizontal) 
+        //each loop will decrease the value for X, moving it to the LEFT towards ZERO
         sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 5
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        //sprite.set function  (Y, value) will set a value for Y (vertical) 
+        //each loop will decrease the value for Y, moving it UPWARDS towards ZERO
         sprite.set(LedSpriteProperty.Y, loopCounter)
         loopCounter = loopCounter - 1
         basic.pause(500)
     }
     sprite.delete()
+
+
+    basic.showIcon(IconNames.Happy)
+})
+
+//When the button B is pressed, the while loops will function, moving the pixel sprite.
+//It is moved in a counterclockwise fashion
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        //sprite.set function  (X, value) will set a value for X (horizontal) 
+        //each loop will increment the value for X, moving it to the RIGHT towards FIVE
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 0
+    sprite = game.createSprite(4, 5)
+    while (loopCounter <= 5) {
+        //sprite.set function  (Y, value) will set a value for Y (vertical) 
+        //each loop will increment the value for Y, moving it DOWNWARDS towards FIVE
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 5
+    sprite = game.createSprite(4, 4)
+    while (loopCounter >= 0) {
+        //sprite.set function  (X, value) will set a value for X (horizontal) 
+        //each loop will decrease the value for X, moving it to the LEFT towards ZERO
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+    loopCounter = 5
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        //sprite.set function  (Y, value) will set a value for Y (vertical) 
+        //each loop will decrease the value for Y, moving it UPWARDS towards ZERO
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+
+
     basic.showIcon(IconNames.Happy)
 })
 
